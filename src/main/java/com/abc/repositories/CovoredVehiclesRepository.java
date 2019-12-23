482c815bf98a9f218632ccc1353b3e22a4b6ee27
abdul hadi <hadi286@icloud.com>
2019-12-23 17:34:27 -0500

    entitymanager

--- src/main/java/com/abc/repositories/CovoredVehiclesRepository.java
+++ src/main/java/com/abc/repositories/CovoredVehiclesRepository.java
@@ -1,5 +1,20 @@
 package com.abc.repositories;
 
+import javax.persistence.EntityManager;
+
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.abc.entities.CovoredVehiclesEntity;
+
+@Repository
+@Transactional
 public class CovoredVehiclesRepository {
+	private EntityManager em;
+
+	public void save(CovoredVehiclesEntity vehicle) {
+
+		em.persist(vehicle);
+	}
 
 }
